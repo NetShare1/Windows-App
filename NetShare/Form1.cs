17ad3a490905f98d6f3f336e1ef03fa5bbb24e90
GraflMartin <graflmartin@gmail.com>
2020-09-17 10:26:34 +0200

    made a tasktray application

---
+++ NetShare/Form1.cs
@@ -0,0 +1,45 @@
+﻿using NetShare.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NetShare
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            LostFocus += Form_LostFocus;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (e.CloseReason == CloseReason.UserClosing)
+            {
+                e.Cancel = true;
+                Hide();
+            }
+        }
+
+        /*private void Form1_Deactivate(object sender, EventArgs e)
+        {
+            // Hide();
+            MessageBox.Show("outside");
+        }*/
+
+        private void Form_LostFocus(object sender, EventArgs e)
+        {
+            Hide();
+        }
+
+    }
+}
---
+++ NetShare/NewApplicationContext.cs
@@ -0,0 +1,61 @@
+﻿using NetShare.Properties;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NetShare
+{
+    public class NewApplicationContext : ApplicationContext
+    {
+        private Form1 Form = new Form1();
+        private NotifyIcon trayIcon;
+
+        public NewApplicationContext()
+        {
+            // Initialize Tray Icon
+            trayIcon = new NotifyIcon()
+            {
+                Icon = Resources.Icon1,
+                ContextMenu = new ContextMenu(new MenuItem[] {
+                new MenuItem("Exit", Exit)
+            }),
+                Visible = true
+            };
+
+            this.trayIcon.Text = "NetShare";
+            this.trayIcon.Click += new System.EventHandler(this.notifyIcon_Click);
+        }
+
+        void Exit(object sender, EventArgs e)
+        {
+            // Hide tray icon, otherwise it will remain shown until user mouses over it
+            trayIcon.Visible = false;
+
+            Application.Exit();
+        }
+
+        private void notifyIcon_Click(object sender, EventArgs e)
+        {
+            var eventArgs = e as MouseEventArgs;
+            if (eventArgs.Button == MouseButtons.Left)
+            {
+                if (Form.Visible == true)
+                {
+                    Form.Hide();
+                }
+                else
+                {
+                    //Application.OpenForms[Form].Focus();
+                    Form.Activate();
+                    Form.Focus();
+                    Form.Show();
+                }
+            }
+        }
+    }
+}
